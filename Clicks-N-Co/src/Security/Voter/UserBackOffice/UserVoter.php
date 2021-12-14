701bcc7658726469b6a9df881f925f79a9dec597
JonaPlaz <jplazanet@hotmail.com>
2021-12-14 11:54:32 +0100

    fix error message

--- Clicks-N-Co/src/Security/Voter/UserBackOffice/UserVoter.php
+++ Clicks-N-Co/src/Security/Voter/UserBackOffice/UserVoter.php
@@ -12,70 +12,59 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 class UserVoter extends Voter
 {
 
-    public function __construct(TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager)
-{
+  public function __construct(TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager)
+  {
     $this->jwtManager = $jwtManager;
     $this->tokenStorageInterface = $tokenStorageInterface;
+  }
+  protected function supports(string $attribute, $subject): bool
+  {
+    // replace with your own logic
+    // https://symfony.com/doc/current/security/voters.html
+    return in_array($attribute, ['READ', 'EDIT', 'ADD'])
 
-}
-    protected function supports(string $attribute, $subject): bool
-    {
-        // replace with your own logic
-        // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, ['READ', 'EDIT', 'ADD'])
+      && $subject instanceof \App\Entity\User;
+  }
 
-            && $subject instanceof \App\Entity\User; 
+  protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+  {
+
+    $decodedJwtToken = $this->jwtManager->decode($this->tokenStorageInterface->getToken());
+
+    $user = $decodedJwtToken->getUser();
+
+
+    // if the user is anonymous, do not grant access
+    if (!$user instanceof UserInterface) {
+      return false;
     }
 
-    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
-    {
-
-        $decodedJwtToken = $this->jwtManager->decode($this->tokenStorageInterface->getToken());
-
-        $user = $decodedJwtToken->getUser();
-
-        
-        // if the user is anonymous, do not grant access
-        if (!$user instanceof UserInterface) {
-            return false;
+    // ... (check conditions and return true to grant permission) ...
+    switch ($attribute) {
+      case 'READ':
+        // logic to determine if the user can EDIT
+        // return true or false
+        if ($subject->getUser() === $user) {
+          return true;
         }
 
-
-
-        // ... (check conditions and return true to grant permission) ...
-        switch ($attribute) {
-            case 'READ':
-                // logic to determine if the user can EDIT
-                // return true or false
-                if($subject->getUser() === $user) {
-                    return true;
-                }
-
-
-                break;
-            case 'EDIT':
-                // logic to determine if the user can EDIT
-                // return true or false
-                if($subject->getUser() === $user) {
-                    return true;
-                }
-
-
-                break;
-            case 'ADD':
-                // logic to determine if the user can EDIT
-                // return true or false
-                if($subject->getUser() === $user) {
-                    return true;
-
-                }
-
-
-
-         
-
+        break;
+      case 'EDIT':
+        // logic to determine if the user can EDIT
+        // return true or false
+        if ($subject->getUser() === $user) {
+          return true;
         }
 
-        return false;
+        break;
+      case 'ADD':
+        // logic to determine if the user can EDIT
+        // return true or false
+        if ($subject->getUser() === $user) {
+          return true;
+        }
     }
+
+    return false;
+  }
 }
