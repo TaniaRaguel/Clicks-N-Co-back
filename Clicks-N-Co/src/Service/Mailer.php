93f1664a8a8ad8fb42e12a45436e9564e3ee9bbf
JonaPlaz <jplazanet@hotmail.com>
2021-12-17 16:53:20 +0100

    feat(css mailer) : wip css twig mailer

--- Clicks-N-Co/src/Service/Mailer.php
+++ Clicks-N-Co/src/Service/Mailer.php
@@ -4,8 +4,8 @@ namespace App\Service;
 
 use App\Entity\Order;
 use App\Entity\User;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 
 class Mailer
 {
@@ -18,27 +18,31 @@ class Mailer
 
   public function sendEmailNewUser(User $user)
   {
-    
+
     $firstName = $user->getFirstname();
-    $lastname = $user->getLastname();
+    $lastName = $user->getLastname();
     $userEmail = $user->getEmail();
 
 
-    $email = (new Email())
+    $email = (new TemplatedEmail())
       ->from('clicksnco@gmail.com')
       ->to('clicksnco-d601f7@inbox.mailtrap.io')
       ->cc($userEmail)
-      ->subject('Bienvenue sur Clicks N Co')
-      // ->text('Allez voir dans votre espace magasin ! Une nouvelle commande est arrivée !')
-      ->html('<h1>Bienvenue sur Clicks N Co, <strong>' . $firstName . ' ' . $lastname . '</strong> !</h1><br><p>Félicitations pour la création de votre compte !</p><br><p>Vous pouvez, dès à présent, passer une commande !</p><br><p>À bientôt,</p><p>L\'équipe <strong>Clicks N Co</p></strong>');
-
+      ->subject('Bienvenue sur Clicks N Co !')
+      ->embedFromPath('images/logo/icon.png', 'logo')
+      ->htmlTemplate('emails/new_user.html.twig')
+      ->context([
+        'logo' => 'logo',
+        'firstname' => $firstName,
+        'lastname' => $lastName,
+      ]);
     $this->mailer->send($email);
   }
   public function sendEmailNewOrderTrader(Order $order)
   {
     $user = $order->getUser();
     $firstName = $user->getFirstname();
-    $lastname = $user->getLastname();
+    $lastName = $user->getLastname();
 
     $orderId = $order->getId();
 
@@ -46,13 +50,20 @@ class Mailer
     $shopEmail = $shop->getEmail();
     $shopName = $shop->getName();
 
-    $email = (new Email())
+    $email = (new TemplatedEmail())
       ->from('clicksnco@gmail.com')
       ->to('clicksnco-d601f7@inbox.mailtrap.io')
       ->cc($shopEmail)
       ->subject('Nouvelle commande Clicks N Co !')
-      // ->text('Allez voir dans votre espace magasin ! Une nouvelle commande est arrivée !')
-      ->html('<h1>Nouvelle commande <strong>REF' . $orderId . '</strong> !</h1><br><h2>Bonjour ' . $shopName . ',</h2><br><p>Nous avons enregistré la commande <strong>REF' . $orderId . '</strong> pour votre client <strong>' . $firstName . ' ' . $lastname . '</strong>.</p><br><p>Vous pouvez, dès à présent, retrouver le détail de votre commande dans votre espace espace magasin.</p><br><p>À bientôt,</p><p>L\'équipe <strong>Clicks N Co</p></strong>');
+      ->embedFromPath('images/logo/icon.png', 'logo')
+      ->htmlTemplate('emails/new_user.html.twig')
+      ->context([
+        'logo' => 'logo',
+        'firstname' => $firstName,
+        'lastname' => $lastName,
+      ]);
+      
+      // ->html('<h1>Nouvelle commande <strong>REF' . $orderId . '</strong> !</h1><br><h2>Bonjour ' . $shopName . ',</h2><br><p>Nous avons enregistré la commande <strong>REF' . $orderId . '</strong> pour votre client <strong>' . $firstName . ' ' . $lastname . '</strong>.</p><br><p>Vous pouvez, dès à présent, retrouver le détail de votre commande dans votre espace espace magasin.</p><br><p>À bientôt,</p><p>L\'équipe <strong>Clicks N Co</p></strong>');
 
     $this->mailer->send($email);
   }
@@ -61,7 +72,7 @@ class Mailer
   {
     $user = $order->getUser();
     $firstName = $user->getFirstname();
-    $lastname = $user->getLastname();
+    $lastName = $user->getLastname();
     $userEmail = $user->getEmail();
 
     $orderId = $order->getId();
@@ -70,13 +81,20 @@ class Mailer
     $shop = $order->getShop();
     $shopName = $shop->getName();
 
-    $email = (new Email())
+    $email = (new TemplatedEmail())
       ->from('clicksnco@gmail.com')
       ->to('clicksnco-d601f7@inbox.mailtrap.io')
       ->cc($userEmail)
       ->subject('Récupitulatif de votre commande Click N Co !')
-      // ->text('Votre commande a été transmise à votre commerçant')
-      ->html('<h1>Votre commande <strong>REF' . $orderId . '</strong> est transmise à <strong>' . $shopName . '</strong> !</h1><br><h2>Bonjour <strong>' . $firstName . ' ' . $lastname . '</strong>,</h2><br><p>Votre commande <strong>REF' . $orderId . '</strong> a bien été transmise à <strong>' . $shopName . '</strong>.</p><br><p>Vous pouvez, dès à présent, retrouver le détail de votre commande dans votre espace espace client.</p><br><h2>Récapitulatif de votre commande</h2><br><p><strong><span style="margin-right: 50px">REF' . $orderId . '</span><span style="margin-right: 50px">' . $shopName . '</span><span style="margin-right: 50px">Prix Total ' . $totalPrice . ' Euros</span></strong></p><br><p>À bientôt,</p><p>L\'équipe <strong>Clicks N Co</strong></p>');
+      ->embedFromPath('images/logo/icon.png', 'logo')
+      ->htmlTemplate('emails/new_user.html.twig')
+      ->context([
+        'logo' => 'logo',
+        'firstname' => $firstName,
+        'lastname' => $lastName,
+      ]);
+     
+      // ->html('<h1>Votre commande <strong>REF' . $orderId . '</strong> est transmise à <strong>' . $shopName . '</strong> !</h1><br><h2>Bonjour <strong>' . $firstName . ' ' . $lastname . '</strong>,</h2><br><p>Votre commande <strong>REF' . $orderId . '</strong> a bien été transmise à <strong>' . $shopName . '</strong>.</p><br><p>Vous pouvez, dès à présent, retrouver le détail de votre commande dans votre espace espace client.</p><br><h2>Récapitulatif de votre commande</h2><br><p><strong><span style="margin-right: 50px">REF' . $orderId . '</span><span style="margin-right: 50px">' . $shopName . '</span><span style="margin-right: 50px">Prix Total ' . $totalPrice . ' Euros</span></strong></p><br><p>À bientôt,</p><p>L\'équipe <strong>Clicks N Co</strong></p>');
 
     $this->mailer->send($email);
   }
@@ -85,7 +103,7 @@ class Mailer
   {
     $user = $order->getUser();
     $firstName = $user->getFirstname();
-    $lastname = $user->getLastname();
+    $lastName = $user->getLastname();
     $userEmail = $user->getEmail();
 
     $orderId = $order->getId();
@@ -97,13 +115,20 @@ class Mailer
     $shopZipCode = $shop->getZipCode();
     $shopCity = $shop->getCity();
 
-    $email = (new Email())
+    $email = (new TemplatedEmail())
       ->from('clicksnco@gmail.com')
       ->to('clicksnco-d601f7@inbox.mailtrap.io')
       ->cc($userEmail)
       ->subject('Votre commande est prête !')
-      // ->text('Votre commande est prête ! Vous pouvez la retirer !')
-      ->html('<h1>Votre commande <strong>REF' . $orderId . '</strong> est disponible !</h1><br><h2>Bonjour <strong>' . $firstName . ' ' . $lastname . '</strong>,</h2><br><p>Votre commande <strong>REF' . $orderId . '</strong> est disponible chez <strong>' . $shopName . '</strong>.</p><br><p>Vous pouvez la retirer.</p><br><h2>Horaires d\'ouvertures et adresse de ' . $shopName . '</h2><br><p><strong>' . $shopOpeningHours . '</strong></p><p><strong>' . $shopName . '</strong></p><p><strong>' . $shopAdress . '</strong></p><p><strong>' . $shopZipCode . ' ' . $shopCity . '</strong></p><br><p>À bientôt,</p><p>L\'équipe <strong>Clicks N Co</strong></p>');
+      ->embedFromPath('images/logo/icon.png', 'logo')
+      ->htmlTemplate('emails/new_user.html.twig')
+      ->context([
+        'logo' => 'logo',
+        'firstname' => $firstName,
+        'lastname' => $lastName,
+      ]);
+      
+      // ->html('<h1>Votre commande <strong>REF' . $orderId . '</strong> est disponible !</h1><br><h2>Bonjour <strong>' . $firstName . ' ' . $lastname . '</strong>,</h2><br><p>Votre commande <strong>REF' . $orderId . '</strong> est disponible chez <strong>' . $shopName . '</strong>.</p><br><p>Vous pouvez la retirer.</p><br><h2>Horaires d\'ouvertures et adresse de ' . $shopName . '</h2><br><p><strong>' . $shopOpeningHours . '</strong></p><p><strong>' . $shopName . '</strong></p><p><strong>' . $shopAdress . '</strong></p><p><strong>' . $shopZipCode . ' ' . $shopCity . '</strong></p><br><p>À bientôt,</p><p>L\'équipe <strong>Clicks N Co</strong></p>');
 
     $this->mailer->send($email);
   }
